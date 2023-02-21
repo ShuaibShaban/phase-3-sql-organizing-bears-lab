57c20aae48150a38c31eb7a30aa8bd477eef972e
shuaib shaban <shuaib>
2023-02-21 15:14:51 +0300

    done test

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears (id, name, age, sex, color, temperament, alive)
+VALUES 
+  (1, 'Mr. Chocolate', 5, 'M', 'Brown', 'Grumpy', true),
+  (2, 'Rowdy', 3, 'M', 'Black', 'Aggressive', true),
+  (3, 'Tabitha', 7, 'F', 'Orange', 'Playful', true),
+  (4, 'Sergeant Brown', 10, 'M', 'Brown', 'Disciplined', true),
+  (5, 'Melissa', 4, 'F', 'White', 'Friendly', true),
+  (6, 'Grinch', 2, 'M', 'Green', 'Sneaky', true),
+  (7, 'Wendy', 6, 'F', 'Brown', 'Adventurous', true),
+  (8, NULL, NULL, NULL, NULL, NULL, false);
